b0c18d58de16dfa7df967e2b59dd1e475350151e
VuagnatV <37271745+VuagnatV@users.noreply.github.com>
2025-06-06 17:09:30 +0200

    finished crud for entreprise and dirigeant

--- src/main/java/com/trady/controller/EntrepriseController.java
+++ src/main/java/com/trady/controller/EntrepriseController.java
@@ -20,8 +20,21 @@ public class EntrepriseController {
         return entrepriseService.getAllEnterprise();
     }
 
+    @GetMapping("/{id}")
+    public Entreprise getById(@PathVariable Long id) { return entrepriseService.getEntrepriseById(id); }
+
     @PostMapping
     public Entreprise create(@RequestBody Entreprise e) {
         return entrepriseService.saveEntreprise(e);
+    }
+
+    @DeleteMapping("/{id}")
+    public Entreprise delete(@PathVariable Long id) {
+        return entrepriseService.deleteEntrepriseById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Entreprise update(@PathVariable Long id, @RequestBody Entreprise e) {
+        return entrepriseService.updateEntreprise(e, id);
     }
 }
